5ab5d32f6376e1968f64d4da29e74fdf0a0828c6
Yucco-K <yukig0527@yahoo.co.jp>
2024-10-19 19:37:01 +0900

    imgをImageに置き換える修正

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   domains: ['paskvjemkyxnpmlrvzzt.supabase.co'],
-  // },
+  images: {
+    domains: ['paskvjemkyxnpmlrvzzt.supabase.co'],
+  },
   images: {
     remotePatterns: [
       {
